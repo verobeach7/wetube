05822081ce3102338caa7fc2defe04162c790651
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2021-02-27 01:26:09 +0900

    Change Profile HTML

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,7 +4,7 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy: searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy });
 };
 export const videos = (req, res) =>
   res.render("videos", { pageTitle: "Videos" });
