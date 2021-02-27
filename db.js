0930d9fd0494a1e813efbdb74e5dea82a8b4c9ac
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2021-02-27 18:11:26 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,62 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 123412,
+    title: "Video super",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 342312,
+    title: "Video Fantastic",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 53321,
+    title: "Video Cool",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 97283,
+    title: "Video Fucking",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+];
