7ea88d5907c07a42e2ce9bc8d272a6de5671c702
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2021-03-01 01:34:59 +0900

    Uploading and Creating a Video Part Two and gitignore the uploads folder

--- app.js
+++ app.js
@@ -13,18 +13,11 @@ const app = express();
 
 app.use(helmet({ contentSecurityPolicy: false }));
 app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
-// app.use(function (req, res, next) {
-//   res.setHeader(
-//     "Content-Security-Policy",
-//     "script-src 'self' https://archive.org"
-//   );
-//   return next();
-// });
 
 app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
